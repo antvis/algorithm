759aad38fd47e80fb2b22c15716581a493974f6d
zqqcee <zqqcee@163.com>
2023-09-08 17:24:31 +0800

    feat: v5 algorithm dfs

---
+++ packages/graph/src/dfs.ts
@@ -0,0 +1,52 @@
+import { Graph, IAlgorithmCallbacks, NodeID } from './types';
+
+function initCallbacks(callbacks: IAlgorithmCallbacks = {} as IAlgorithmCallbacks) {
+  const initiatedCallback = callbacks;
+  const stubCallback = () => { };
+  const allowTraversalCallback = () => true;
+  initiatedCallback.allowTraversal = callbacks.allowTraversal || allowTraversalCallback;
+  initiatedCallback.enter = callbacks.enter || stubCallback;
+  initiatedCallback.leave = callbacks.leave || stubCallback;
+
+  return initiatedCallback;
+}
+
+function depthFirstSearchRecursive(
+  graph: Graph,
+  currentNodeId: NodeID,
+  previousNodeId: NodeID,
+  callbacks: IAlgorithmCallbacks,
+  visit: Set<NodeID>
+) {
+  callbacks.enter({
+    current: currentNodeId,
+    previous: previousNodeId,
+  });
+  graph.getNeighbors(currentNodeId).forEach((nextNode) => {
+    const nextNodeId = nextNode.id;
+    if (
+      callbacks.allowTraversal({
+        previous: previousNodeId,
+        current: currentNodeId,
+        next: nextNodeId,
+      }) && !visit.has(nextNodeId)
+    ) {
+      visit.add(nextNodeId);
+      depthFirstSearchRecursive(graph, nextNodeId, currentNodeId, callbacks, visit);
+    }
+  });
+  callbacks.leave({
+    current: currentNodeId,
+    previous: previousNodeId,
+  });
+}
+
+export default function depthFirstSearch(
+  graph: Graph,
+  startNodeId: NodeID,
+  originalCallbacks?: IAlgorithmCallbacks,
+) {
+  const visit = new Set<NodeID>();
+  visit.add(startNodeId);
+  depthFirstSearchRecursive(graph, startNodeId, '', initCallbacks(originalCallbacks), visit);
+}
